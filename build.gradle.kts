87ee291a25db03a412d25302f57e8b29cae50359
Stanislaff Fedyanin <re1971reg@gmail.com>
2025-11-23 18:29:34 +0300

    replace "${project.buildDir}/reports/jacoco") to "${layout.buildDirectory.asFile.get()}/reports/jacoco") in builder file

--- build.gradle.kts
+++ build.gradle.kts
@@ -126,8 +126,6 @@ tasks.checkstyleTest {
 
 jacoco {
     toolVersion = "0.8.13"
-    //reportsDirectory = layout.buildDirectory.dir("${buildDir}/reports/jacoco")
-    //reportsDirectory = layout.buildDirectory.dir("${project.buildDir}/reports/jacoco")
     reportsDirectory = layout.buildDirectory.dir("${layout.buildDirectory.asFile.get()}/reports/jacoco")
     println("${layout.buildDirectory.asFile.get()}/reports/jacoco")
 }
